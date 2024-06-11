8dbe29ffb9edc39f9c5016ef809365321f51e08d
lucasfm20 <138518985+lucasfm20@users.noreply.github.com>
2024-06-11 19:24:27 -0300

    Update App.js

--- app/App.js
+++ app/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { Text, SafeAreaView, StyleSheet, TextInput, View, TouchableOpacity } from 'react-native';
 
-// You can import supported modules from npm
+
 import { Card } from 'react-native-paper';
 
-// or any files within the Snack
+
 import AssetExample from './components/AssetExample';
 
 export default function App() {
